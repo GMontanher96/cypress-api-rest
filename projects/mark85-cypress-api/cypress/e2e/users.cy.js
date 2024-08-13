f4eb2b60b9381d4c4cd37d8183fc65d17ca8fc4d
Montanher <gabrielmontanherr@gmail.com>
2024-08-13 14:36:34 -0300

    Updates

--- projects/mark85-cypress-api/cypress/e2e/users.cy.js
+++ projects/mark85-cypress-api/cypress/e2e/users.cy.js
@@ -1,5 +1,20 @@
-describe('template spec', () => {
-  it('passes', () => {
-    cy.visit('https://example.cypress.io')
+describe('POST /users', () => {
+
+  it('register a new user', () => {
+
+    const user = {
+      name: 'teste',
+      email: 'gabrielmontanherrr@gmail.com',
+      password: "123456"
+    }
+
+    cy.request({
+      url: '/users',
+      method: 'POST',
+      body: user
+    }).then(response => {
+      expect(response.status).to.eq(200)
+    })
   })
+
 })
