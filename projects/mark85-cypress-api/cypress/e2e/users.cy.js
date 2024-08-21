244b07424d664360d91ad4d3341320f1c10f2387
Montanher <gabrielmontanherr@gmail.com>
2024-08-21 14:08:19 -0300

    Updates

--- projects/mark85-cypress-api/cypress/e2e/users.cy.js
+++ projects/mark85-cypress-api/cypress/e2e/users.cy.js
@@ -1,41 +1,67 @@
-describe('POST /users', () => {
-
-  it('register a new user', () => {
-
+describe("POST /users", () => {
+  it("register a new user", () => {
     const user = {
-      name: 'montanher',
-      email: 'gabrielmontanherr@gmail.com',
-      password: "123456"
-    }
+      name: "montanher",
+      email: "gabrielmontanherr@gmail.com",
+      password: "123456",
+    };
 
-    cy.task('deleteUser', user.email)
+    cy.task("deleteUser", user.email);
 
-    cy.postUser(user)
-      .then(response=> {
-        expect(response.status).to.eq(200)
-      })
+    cy.postUser(user).then((response) => {
+      expect(response.status).to.eq(200);
+    });
+  });
+
+  it("duplicate email", () => {
+    const user = {
+      name: "Gerson Diass",
+      email: "gersonn@gmail.com",
+      password: "123456",
+    };
+    cy.task("deleteUser", user.email);
+
+    cy.postUser(user);
+
+    cy.postUser(user).then((response) => {
+      const { message } = response.body;
+
+      expect(response.status).to.eq(409);
+      expect(message).to.eq("Duplicated email!");
+    });
+  });
+
+  context.only("required fields", () => {
+
+  const user = {
+    name: 'Margot Robbie',
+    email: 'margot@gmail.com',
+    password: '123456'
+  }
+
+  it('name is required', () => {
+  
+    delete user.name
+
+    cy.postUser(user).then((response) => {
+
+    const { message } = response.body
+      expect(response.status).to.eq(400);
+      expect(message).to.eq("ValidationError: \"name\" is required")
+    });
   })
 
-  it('duplicate email', () => {
-    const user = {
-      name: 'Gerson Diass',
-      email: 'gersonn@gmail.com',
-      password: '123456'
-    }
-    cy.task('deleteUser', user.email)
+   it('email is required', () => {
+  
+    delete user.email
 
-    cy.postUser(user)
+    cy.postUser(user).then((response) => {
 
-    cy.postUser(user)
-      .then(response=> {
-
-        const { message } = response.body
-
-        expect(response.status).to.eq(409)
-        expect(message).to.eq('Duplicated email!')
-      })
+  
+      expect(response.status).to.eq(400);
+   
+    });
   })
-})
 
-
-
+  });
+});
