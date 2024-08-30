d506fc5c830df665198945f49a022d04e50b867c
Montanher <gabrielmontanherr@gmail.com>
2024-08-30 15:31:33 -0300

    Updates

---
+++ projects/mark85-cypress-api/cypress/e2e/tasks.cy.js
@@ -0,0 +1,26 @@
+describe('POST /tasks', () => {
+
+    beforeEach(function () {
+        cy.fixture('tasks'.then(function (tasks) {
+            this.tasks = tasks
+        }))
+    })
+
+    it('register a new task', function () {
+        const { user, task } = this.tasks.create
+
+        cy.postSession(user)
+        .then(response => {
+            cy.log(response.body.token)
+        })
+
+        cy.api({
+            url: '/tasks',
+            method: 'POST',
+            body: task,
+            failOnStatusCode: false
+        }).then(response => {
+            expect(response.status).to.eq(200)
+        })
+    })
+})
